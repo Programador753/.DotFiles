37c6ab42b7a86ad83230610a54e73552356d5925
Programador753 <antoniohcavero@outlook.com>
2025-01-11 15:50:47 +0100

    mejoras

--- install.sh
+++ install.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 sudo apt update
-# esto seguro que funciona
-sudo apt install curl -y
-curl https://bootstrap.pypa.io/get-pip.py -o $HOME/get-pip.py  
-sudo python3 $HOME/get-pip.py 
-pip3 install ansible
-pip3 install ansible-base
-sudo apt install ansible -y
-if [ -z "$CODESPACE_NAME" ]; then 
-  ansible-playbook ./Vagrant/localhost-deploy.yml --ask-become-pass
-else
-  ansible-playbook ./Vagrant/minimal-deploy.yml
-fi
 
 # Instalar GitHub CLI (gh) y github-copilot-cli
 (type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
@@ -69,6 +57,16 @@ else
     echo "nvm ya está instalado"
 fi
 
+
+#!/usr/bin/env bash
+sudo apt update
+# esto seguro que funciona
+sudo apt install curl -y
+curl https://bootstrap.pypa.io/get-pip.py -o $HOME/get-pip.py  
+sudo python3 $HOME/get-pip.py 
+pip3 install ansible
+pip3 install ansible-base
+sudo apt install ansible -y
 if [ -z "$CODESPACE_NAME" ]; then 
   ansible-playbook ./Vagrant/localhost-deploy.yml --ask-become-pass
 else
